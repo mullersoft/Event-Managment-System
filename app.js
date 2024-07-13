8c2b97aaf07291b3eb234e610b114b7b96c1a5a0
mulugetali <mulerselinger@gmail.com>
2024-07-13 21:52:16 +0300

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.use(express.json());
 //  Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/v1/event", eventRoute);
+//Handling Unhandled Routes
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: "`Can't find ${req.originalUrl} on this server!`",
+  });
+  //   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 // Export the app for use in other files
 module.exports = app;
