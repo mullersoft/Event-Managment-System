3cdab32dc1bd11f4c2080367ca31a75e2d49dcb2
mulugetali <mulerselinger@gmail.com>
2024-07-26 17:14:09 +0300

    Adding a me Endpoint

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -17,6 +17,10 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: { users },
   });
 });
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id
+  next()
+}
 exports.updateMe = catchAsync(async (req, res, next) => {
   //1)create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
